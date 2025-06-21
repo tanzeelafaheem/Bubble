6f6c91a7f6c81e89ad0ef4983ea693247de22ff0
tanzeelafaheem <tanzeelafaheem20@gmail.com>
2025-06-21 22:12:43 +0530

    feat: logout

--- client/src/components/Shared/Navbar.jsx
+++ client/src/components/Shared/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate=useNavigate();
+  const logOut=async()=>{
+    localStorage.clear();
+    navigate('/');
+  }
   return (
+    <>
     <header className="flex items-center justify-between border-b border-gray-200 px-10 py-3 bg-white">
       <div className="flex items-center gap-4 text-gray-900">
         <div className="text-2xl font-bold">🫧 Bubble</div>
@@ -9,16 +16,17 @@ const Navbar = () => {
       <div className="flex items-center gap-8">
         <a className="text-sm font-medium" href="#">Home</a>
         <a className="text-sm font-medium" href="#">Profile</a>
-        <a className="text-sm font-medium" href="#">Logout</a>
+        <a className="text-sm font-medium cursor-pointer" onClick={logOut}>Logout</a>
         <a className="text-sm font-medium" href="#">Settings</a>
         <div
           className="bg-center bg-no-repeat bg-cover rounded-full w-10 h-10"
           style={{
             backgroundImage: "url('https://i.pravatar.cc/40?img=1')"
           }}
-        />
+          />
       </div>
     </header>
+    </>
   );
 };
 
