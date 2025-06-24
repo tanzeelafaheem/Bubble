e91f600ea174def62266588e3f625064e52d7086
tanzeelafaheem <tanzeelafaheem20@gmail.com>
2025-06-24 22:24:02 +0530

    updated axiosInstance

--- client/src/api/axiosInstance.jsx
+++ client/src/api/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://bubble-server-bcp9.onrender.com/api',
+  baseURL: 'https://bubble-server-leky.onrender.com/api',
 });
 
 export default axiosInstance
