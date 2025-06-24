603bf0afbe7f0c5b90aea449928d48d4dc4f8ed2
tanzeelafaheem <tanzeelafaheem20@gmail.com>
2025-06-24 20:06:38 +0530

    ui changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
-import Test from './components/Others/Test';
 import Profile from './components/Others/Profile'
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <Route path="/" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route path="/home" element={<Home />} />
-      <Route path='/testing' element={<Test/>}/>
       <Route path='/profile' element={<Profile/>}/>
     </Routes>
   );
