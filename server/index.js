0a5eb8b53ae27dc969e400c6f5baed281bd0ea07
tanzeelafaheem <tanzeelafaheem20@gmail.com>
2025-06-24 22:33:54 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -16,9 +16,10 @@ const PORT = process.env.PORT || 5000;
 connectDb();
 
 app.use(cors({
-  origin: true, 
-  credentials: true,
+  origin: "*",  
+  credentials: false, 
 }));
+
 app.use(express.json());
 
 // Routes
